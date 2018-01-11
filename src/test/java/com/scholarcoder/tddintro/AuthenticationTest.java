c996762852df5c2607271312378c4f5eb4894a10
Adrian van den Houten <adrianvdh@gmail.com>
2018-01-11 06:42:08 +0200

    Added first test

--- src/test/java/com/scholarcoder/tddintro/AuthenticationTest.java
+++ src/test/java/com/scholarcoder/tddintro/AuthenticationTest.java
@@ -1,4 +1,25 @@
 package com.scholarcoder.tddintro;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AuthenticationTest {
+
+    @Test
+    public void userLoginWithCorrectCredentials() throws Exception {
+
+        // Given that this user exists
+        String username = "adrianvdh";
+        String password = "hello123";
+        AuthorisationContext authorizationContext = new AuthorisationContext();
+
+        // When authenticating
+        authorizationContext.authenticate(username, password);
+
+        // Then the session must be valid
+        Session userSession = Session.getCurrentSession();
+        Assert.assertEquals("adrianvdh", userSession.getAuthenticatedUser().username);
+        Assert.assertEquals("hello123", userSession.getAuthenticatedUser().password);
+
+    }
 }
