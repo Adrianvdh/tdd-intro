022db660bdd3fd8c83e83cdc91375d3f58151207
Adrian van den Houten <adrianvdh@gmail.com>
2018-01-15 07:06:09 +0200

    Replaced mock foundUser with collection

--- src/main/java/com/scholarcoder/tddintro/UserService.java
+++ src/main/java/com/scholarcoder/tddintro/UserService.java
@@ -1,12 +1,21 @@
 package com.scholarcoder.tddintro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
     public Session session;
 
+    List<User> users = new ArrayList<>();
+    {
+        User user = new User();
+        user.username = "adrianvdh";
+        user.password = "hello123";
+        users.add(user);
+    }
+
     public void login(String username, String password) {
-        User foundUser = new User();
-        foundUser.username = username;
-        foundUser.password = password;
+        User foundUser = users.get(0);
 
         if(foundUser.username.equals(username) && foundUser.password.equals(password)) {
             session = new Session();
