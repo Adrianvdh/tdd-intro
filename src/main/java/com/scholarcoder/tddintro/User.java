244826b26194425545e9afea7a66397d8814b51a
Adrian van den Houten <adrianvdh@gmail.com>
2018-01-15 06:46:52 +0200

    Added authentication login in userService with mock foundUser

--- src/main/java/com/scholarcoder/tddintro/User.java
+++ src/main/java/com/scholarcoder/tddintro/User.java
@@ -1,6 +1,6 @@
 package com.scholarcoder.tddintro;
 
 public class User {
-    String username = "adrianvdh";
-    String password = "hello123";
+    String username;
+    String password;
 }
--- src/main/java/com/scholarcoder/tddintro/UserService.java
+++ src/main/java/com/scholarcoder/tddintro/UserService.java
@@ -4,7 +4,15 @@ public class UserService {
     public Session session;
 
     public void login(String username, String password) {
-        session = new Session();
-        session.user = new User();
+        User foundUser = new User();
+        foundUser.username = username;
+        foundUser.password = password;
+
+        if(foundUser.username.equals(username) && foundUser.password.equals(password)) {
+            session = new Session();
+            session.user = new User();
+            session.user.username = foundUser.username;
+            session.user.password = foundUser.password;
+        }
     }
 }
