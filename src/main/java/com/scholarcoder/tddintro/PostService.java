880d9454ce5245345992fa3f7ae9f1d82ee765dc
Adrian van den Houten <adrianvdh@gmail.com>
2018-01-20 09:14:00 +0200

    Added preAuthorize check on publish method

--- src/main/java/com/scholarcoder/tddintro/PostService.java
+++ src/main/java/com/scholarcoder/tddintro/PostService.java
@@ -3,11 +3,14 @@ package com.scholarcoder.tddintro;
 public class PostService {
 
     private Post post;
+    private UserService userService;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserService userService) {
+        this.userService = userService;
     }
 
     public void publish(Post postToPublish) {
+        this.userService.preAuthorize();
         this.post = postToPublish;
         this.post.published = true;
     }
