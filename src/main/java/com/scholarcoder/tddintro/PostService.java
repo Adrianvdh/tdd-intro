34b76dc14f08a16a4f2e998742e996131dbf5704
Adrian van den Houten <adrianvdh@gmail.com>
2018-02-14 07:06:18 +0200

    Added postRepository as argument to postService contructor

--- src/main/java/com/scholarcoder/tddintro/PostService.java
+++ src/main/java/com/scholarcoder/tddintro/PostService.java
@@ -1,14 +1,14 @@
 package com.scholarcoder.tddintro;
 
 public class PostService {
-    public Post post;
+    public PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     public void publish(Post post) {
         post.releaseStatus = ReleaseStatus.PUBLISHED;
-        this.post = post;
-    }
-
-    public Post findLatestPublishedPost() {
-        return post;
+        postRepository.save(post);
     }
 }
